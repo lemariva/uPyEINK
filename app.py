f6f69b5af3d43c3f695e1f2821600c32d4458a07
Mauro Riva <lemariva@gmail.com>
2019-12-29 02:02:20 +0100

    name changed and try & catch added

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ import font8
 
 from widgets.forecast import ForecastWidget
 from widgets.news import NewsWidget
-import widgets.notes_server as NoteServer
+import widgets.notes as NoteServer
 
 import settings
 import functions
@@ -53,75 +53,73 @@ epd = e7in5.EPD(rst, dc, busy, cs, sck, mosi, miso)
 def run():
     gc.collect()
     while True:
-        epd.init_v1()
-        epd.clear_frame()
-        gc.collect()
-        print("loading data...")
-        ## Weather Section
-        weather_widget = ForecastWidget(api_key=settings.weather_api_key, location=settings.location)
-        weather = weather_widget.get_data()
-        weather_widget = None
+        try:
+            epd.init_v1()
+            epd.clear_frame()
+            ## Weather Section
+            weather_widget = ForecastWidget(api_key=settings.weather_api_key, location=settings.location)
+            weather = weather_widget.get_data()
+            weather_widget = None
 
-        epd.display_string_at(5, 5, weather['city']['name'], font16, e7in5.UNCOLORED)
-        epd.display_string_at(5, 25, "Sunrise: ", font16, e7in5.UNCOLORED)
-        epd.display_string_at(100, 25, functions.seconds_to_text(weather['city']['sunrise'], weather['city']['tz']), font16, e7in5.COLORED)
-        epd.display_string_at(5, 40, "Sunset: ", font16, e7in5.UNCOLORED)
-        epd.display_string_at(100, 40, functions.seconds_to_text(weather['city']['sunset'], weather['city']['tz']), font16, e7in5.COLORED)
+            epd.display_string_at(5, 5, weather['city']['name'], font16, e7in5.UNCOLORED)
+            epd.display_string_at(5, 25, "Sunrise: ", font16, e7in5.UNCOLORED)
+            epd.display_string_at(100, 25, functions.seconds_to_text(weather['city']['sunrise'], weather['city']['tz']), font16, e7in5.COLORED)
+            epd.display_string_at(5, 40, "Sunset: ", font16, e7in5.UNCOLORED)
+            epd.display_string_at(100, 40, functions.seconds_to_text(weather['city']['sunset'], weather['city']['tz']), font16, e7in5.COLORED)
 
-        x_move = 110
-        for index in weather['next']:
-            forecast = weather['next'][index]
-            #print(forecast)
-            epd.display_string_at(200 + x_move * index, 5, functions.seconds_to_text(forecast['dt'], weather['city']['tz']), font16, e7in5.COLORED)
-            epd.display_string_at(200 + x_move * index, 20, forecast['main'], font16, e7in5.UNCOLORED)
-            epd.display_string_at(200 + x_move * index, 35, forecast['temp'], font8, e7in5.UNCOLORED)
-            epd.display_string_at(200 + x_move * index, 45, forecast['temp_dt'], font8, e7in5.UNCOLORED)
-            epd.display_string_at(200 + x_move * index, 55, forecast['wind'], font8, e7in5.UNCOLORED)
-            epd.display_string_at(200 + x_move * index, 65, forecast['press'], font8, e7in5.UNCOLORED)
-            epd.draw_vertical_line(300 + x_move * index, 5, 65, e7in5.COLORED)
+            x_move = 110
+            for index in weather['next']:
+                forecast = weather['next'][index]
+                #print(forecast)
+                epd.display_string_at(200 + x_move * index, 5, functions.seconds_to_text(forecast['dt'], weather['city']['tz']), font16, e7in5.COLORED)
+                epd.display_string_at(200 + x_move * index, 20, forecast['main'], font16, e7in5.UNCOLORED)
+                epd.display_string_at(200 + x_move * index, 35, forecast['temp'], font8, e7in5.UNCOLORED)
+                epd.display_string_at(200 + x_move * index, 45, forecast['temp_dt'], font8, e7in5.UNCOLORED)
+                epd.display_string_at(200 + x_move * index, 55, forecast['wind'], font8, e7in5.UNCOLORED)
+                epd.display_string_at(200 + x_move * index, 65, forecast['press'], font8, e7in5.UNCOLORED)
+                epd.draw_vertical_line(300 + x_move * index, 5, 65, e7in5.COLORED)
 
-        epd.draw_horizontal_line(1, 75, 640, e7in5.COLORED)
-        gc.collect()
-
-        ## News Section
-        news_widget = NewsWidget(api_key=settings.news_api_key, country=settings.news_country)
-        news = news_widget.get_data()
-        news_widget = None
-
-        y_move = 30
-        if news['status'] == 'ok':
-            for index, article in enumerate(news['articles']):
-                epd.display_string_at(1, 80 + y_move * index,
-                                    functions.replace_unicode(article['title']), font16, e7in5.COLORED)
-                epd.display_string_at(5, 95 + y_move * index,
-                                    functions.replace_unicode(article['description']), font8, e7in5.UNCOLORED)
-
-        epd.draw_horizontal_line(1, 205, 640, e7in5.COLORED)
-        gc.collect()
-
-        ## Notes Section
-        get_keys = NoteServer.Note.get_keys()
-        note_idx = 0
-        y_move = 25
-        x_move = 320
-        for key in get_keys:
-            note = NoteServer.Note.get_id(key)[0]
-            if note.archived == 0:
-                epd.display_string_at(1 + x_move * (note_idx > 5), 210 + y_move * (note_idx - (note_idx > 5) * 6),
-                                    functions.replace_unicode(note.timestamp), font8, e7in5.COLORED)
-                epd.display_string_at(1 + x_move * (note_idx > 5), 218 + y_move * (note_idx - (note_idx > 5) * 6),
-                                    functions.replace_unicode(note.content), font16, e7in5.UNCOLORED)
-                note_idx = note_idx + 1
+            epd.draw_horizontal_line(1, 75, 640, e7in5.COLORED)
             gc.collect()
-        get_keys = None
-        note = None
-        gc.collect()
 
-        ## Display Update
-        print("updating display...")
-        epd.display_string_at(1, 375, "updated: %d-%02d-%02d %02d:%02d:%02d" % utime.localtime()[:6], font8, e7in5.COLORED)
-        epd.display_frame()
-        print("updated...")
-        gc.collect()
-        utime.sleep_ms(DELAY_TIME)
+            ## News Section
+            news_widget = NewsWidget(api_key=settings.news_api_key, country=settings.news_country)
+            news = news_widget.get_data()
+            news_widget = None
+
+            y_move = 30
+            if news['status'] == 'ok':
+                for index, article in enumerate(news['articles']):
+                    epd.display_string_at(1, 80 + y_move * index,
+                                        functions.replace_unicode(article['title']), font16, e7in5.COLORED)
+                    epd.display_string_at(5, 95 + y_move * index,
+                                        functions.replace_unicode(article['description']), font8, e7in5.UNCOLORED)
+
+            epd.draw_horizontal_line(1, 205, 640, e7in5.COLORED)
+            gc.collect()
+
+            ## Notes Section
+            get_keys = NoteServer.models_btree.Note.get_keys()
+            note_idx = 0
+            y_move = 25
+            x_move = 320
+            for key in get_keys:
+                note = NoteServer.models_btree.Note.get_id(key)[0]
+                if note.archived == 0:
+                    epd.display_string_at(1 + x_move * (note_idx > 5), 210 + y_move * (note_idx - (note_idx > 5) * 6),
+                                        functions.replace_unicode(note.timestamp), font8, e7in5.COLORED)
+                    epd.display_string_at(1 + x_move * (note_idx > 5), 218 + y_move * (note_idx - (note_idx > 5) * 6),
+                                        functions.replace_unicode(note.content), font16, e7in5.UNCOLORED)
+                    note_idx = note_idx + 1
+            get_keys = None
+            note = None
+            gc.collect()
+
+            ## Display Update
+            epd.display_string_at(1, 375, "updated: %d-%02d-%02d %02d:%02d:%02d" % utime.localtime()[:6], font8, e7in5.COLORED)
+            epd.display_frame()
+            gc.collect()
+            utime.sleep_ms(DELAY_TIME)
         
+        except:
+            print("An exception occurred")
